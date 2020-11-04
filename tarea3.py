9b2facd566a3bf8d7643740ff2c977245c7b1992
J-AMD <52175127+J-AMD@users.noreply.github.com>
2020-11-04 14:26:54 -0600

    complete searches
    the code contain the search by country, continent and hemisphere since file text

--- tarea3.py
+++ tarea3.py
@@ -16,7 +16,7 @@ def busquedaPais(pais):
             total += 1
             if(pais in words[3]):
                 linea += 1
-                print(words[4], words[2])
+                print("IATA: {}, Pais: {}" .format(words[4], words[2]))
 
     print(linea,' out of ', total)
     
@@ -32,8 +32,29 @@ def busquedaContinente(contienente):
             total += 1
             if(contienente in words[11]):
                 linea += 1
-                print(words[4], words[11])
+                print("IATA: {}, Continente: {}" .format(words[4], words[11]))
                 
+    print(linea,' out of ', total)
+    
+############################################################---Aeropuertos por hemisferio
+def busquedaHemisferio(hemisferio):
+    linea = 0
+    total = 0
+    latitud = 0
+    
+    for line in fh:
+        line.rstrip()
+        words = line.split(',')
+        latitud = float(words[7])
+        total += 1
+        if hemisferio == 1 and latitud > 0:
+            linea += 1
+            print(words[4], words[11], words[2])
+        if hemisferio == 2 and latitud < 0:
+            linea += 1
+            print("IATA: {}, Continente: {}, Pais: {}" .format(words[4], words[11], words[2]))
+        latitud = 0
+            
     print(linea,' out of ', total)
     
 #################################################################################------Main
@@ -47,7 +68,9 @@ except:
 print('Elija una opcion de busqueda:')
 print('1.-Busqueda por Pais')
 print('2.-Busqueda por Continente')
+print('3.-Busqueda por Hemisferio')
 opcion = int(input('Opcion: '))
+
 
 if opcion == 1:
     pais = input('Escriba el Pais: ')
@@ -55,3 +78,9 @@ if opcion == 1:
 if opcion == 2:
     cont = input("Escriba el continente:")
     busquedaContinente(cont)
+if opcion == 3:
+    print('Elija un hemisferio:')
+    print('\t1.-Norte')
+    print('\t2.-Sur')
+    hemis = int(input('Opcion: '))
+    busquedaHemisferio(hemis)
